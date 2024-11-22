83ecc35e6cac428d53aea8953a2d32f8a508b32b
Anderson Costa <and.neo.@gmail.com>
2024-11-22 14:24:49 -0300

    projeto 7 correcao const  0.0.7

--- scripts/index.js
+++ scripts/index.js
@@ -8,11 +8,11 @@ buttonFecharModal.onclick = function() {
 }
 
 buttonEditar.onclick = function() {
-  let inputName = document.querySelector("#input_popup-name");
-  let inputJob = document.querySelector("#input_popup-job");
+  const inputName = document.querySelector("#input_popup-name");
+  const inputJob = document.querySelector("#input_popup-job");
 
-  let labelName = document.querySelector("#profile__name");
-  let labelDesc = document.querySelector("#profile__description");
+  const labelName = document.querySelector("#profile__name");
+  const labelDesc = document.querySelector("#profile__description");
 
   inputName.value = labelName.textContent;
   inputJob.value = labelDesc.textContent;
@@ -22,7 +22,7 @@ buttonEditar.onclick = function() {
 
 
 // Vamos encontrar o formulário no DOM
-let formElement = document.querySelector("dialog .popup__form");
+const formElement = document.querySelector("dialog .popup__form");
 
 // Em seguida vem o handler do submit
 // ainda não vai enviar para lugar nenhum
@@ -38,17 +38,18 @@ function handleProfileFormSubmit(evt) {
     // Explicaremos em mais detalhes posteriormente.
 
     // Pegue os valores de cada campo do valor da propriedade correspondente
-    let inputName = document.querySelector("#input_popup-name");
-    let inputJob = document.querySelector("#input_popup-job");
+    const inputName = document.querySelector("#input_popup-name");
+    const inputJob = document.querySelector("#input_popup-job");
 
     // Selecione os elementos aos quais os valores dos campos serão inseridos
-    let labelName = document.querySelector("#profile__name");
-    let labelDesc = document.querySelector("#profile__description");
+    const labelName = document.querySelector("#profile__name");
+    const labelDesc = document.querySelector("#profile__description");
 
     // Insira novos valores usando a
     // propriedade textContent
     labelName.textContent = inputName.value;
     labelDesc.textContent = inputJob.value;
+    modal.close();
 }
 
 // Conecte o handler ao formulário:
